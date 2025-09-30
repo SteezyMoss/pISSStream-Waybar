b7d1615660d5258e525ea029c4c3271275dcd35a
Ridge <ridgehumphreys22@gmail.com>
2025-09-30 17:53:19 -0400

    cleaned up code and added comments

--- pISSStream.py
+++ pISSStream.py
@@ -2,55 +2,47 @@ from lightstreamer.client import *
 import time
 import json
 
+# Subscribing to the urine tanks field/item and requesting the initial snapshot
 sub = Subscription("MERGE",["NODE3000005"],["Value"])
 sub.setRequestedSnapshot("yes")
 
+# Setting lazy global variables for tracking tank value and subscription status
 gValue = -1 
 gStatus = "Disconnected"
 
+
+# Defining the subscription listener and configuring necessary signals
 class SubListener(SubscriptionListener):
     def onItemUpdate(self, update): 
         global gValue
         gValue = update.getValue("Value") 
 
-    def onListenStart(self):
-        #print("Listen Started")
-        pass
-
     def onSubscription(self):
-        #print("Subscribed!")
         global gStatus
         gStatus = "Connected"
-
-    def onSubscriptionError(self, code, message):
-        #print(code, message)
-        pass
-
-
-class ClientSpy(ClientListener):
-    def onServerError(self, code, message):
-        #print(f"Spy Error: {code}, {message}")
-        pass
 
 
 sub.addListener(SubListener())
 
 client = LightstreamerClient("https://push.lightstreamer.com", "ISSLIVE")
-client.addListener(ClientSpy())
 client.connect()
 client.subscribe(sub)
 
-time.sleep(3)
+# Added this to ensure the client has time to connect, subscribe, and snapshot before printing
+time.sleep(2)
 
 
+# Checking if gValue received an update from... space, that is an interesting thought
 if gValue == -1:
     p = "N/A"
 else:
-    p = f"{str(gValue)}%"
+    try:
+        p = f"{str(gValue)}%"
+    except Exception:
+        p = "N/A"
 
 
-try:
-    print(json.dumps({"text": p, "tooltip": gStatus}), flush=True)
-except Exception:
-    print(json.dumps({"text": "N/A", "tooltip": "N/A"}))
-    
+# Output p and gStatus as a json for waybar
+print(json.dumps({"text": p, "tooltip": gStatus}), flush=True)
+
+
